c1924c6407f3a25deec9347cea141b4820360ce8
Asrani-Aman <amanasrani1712@gmail.com>
2024-06-03 15:14:44 +0530

    removing syntax error in omniauth_callback_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def oktaoauth
        @user = User.from_omniauth(request.env["omniauth.auth"])
-       sign_in(@user)r
+       sign_in(@user)
        redirect_to root_path
     end
   end
